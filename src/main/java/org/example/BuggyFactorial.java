40d2c81ff5f2cfbb7c505277d94bb2d379619dd8
Stefan Adrian Huma <stefanadrian.huma@alumni.uniroma2.eu>
2023-04-04 11:10:18 +0200

    Modified the factorial to see if the build will test

--- src/main/java/org/example/BuggyFactorial.java
+++ src/main/java/org/example/BuggyFactorial.java
@@ -5,9 +5,9 @@ import static java.lang.Math.abs;
 public class BuggyFactorial {
     public int myBuggyFact(int p){
         int result = abs(p);
-        if(p == 0){
+        /*if(p == 0){
             return 1;
-        }
+        }*/
         if (result > 1){
             int next = result-1;
             result = result*myBuggyFact(next);
