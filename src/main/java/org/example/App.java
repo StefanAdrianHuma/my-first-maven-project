103ad02760b3a6031995719e521ee1fc2ef85e09
Stefan Adrian Huma <stefanadrian.huma@alumni.uniroma2.eu>
2023-04-04 09:54:13 +0200

    Added "Again" at hello World in order ti see if the workflows starts

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World! Again!" );
+        BuggyFactorial fact = new BuggyFactorial();
+        System.out.println(fact.myBuggyFact(10));
     }
 }
--- src/test/java/org/example/BuggyFactorialTest.java
+++ src/test/java/org/example/BuggyFactorialTest.java
@@ -1,4 +1,16 @@
+package org.example;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
+
 public class BuggyFactorialTest {
-  
+
+    @Test
+    public void myBuggyFact() {
+        BuggyFactorial fact = new BuggyFactorial();
+        assertEquals(24,fact.myBuggyFact(4));
+        assertEquals(1,fact.myBuggyFact(0));
+    }
 }
